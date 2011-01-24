6324769d9044f8cdae9252637a4fc8e873e303c5
DevDevilDotNL <Johan.overeem@gmail.com>
2011-01-24 07:55:40 +0100

    view

---
+++ views/form.php
@@ -0,0 +1,16 @@
+<ol>
+	<li class="even">
+		<label>URL</label>
+		<?php echo form_input('url', $options['url']); ?>
+	</li>
+	<li class="even">
+		<label>Width</label>
+		<?php echo form_input('width', $options['width']); ?>
+                Px
+	</li>
+        <li class="even">
+		<label>Height</label>
+		<?php echo form_input('height', $options['height']); ?>
+                Px
+	</li>
+</ol>
